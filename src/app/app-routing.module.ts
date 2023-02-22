d9c90c5045f0724ce3d5ff99f5a1e910a2378afe
Mind! <maikmj101@gmail.com>
2023-02-22 12:47:47 -0500

    Design APP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GraphsComponent } from './pages/graphs/graphs.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'graphs', component: GraphsComponent },
+      { path: 'progress', component: ProgressComponent},
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ]
+  },
+  
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  
+  { path: '**', component: PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/graphs/graphs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphs',
+  templateUrl: './graphs.component.html',
+  styles: [
+  ]
+})
+export class GraphsComponent {
+
+}
